e4675e37dee8c83fecf755dbfe7617808c527d00
Bawb <meancraft@hotmail.com>
2015-05-01 11:16:48 -0700

    Final Android Build
    Finalized for Android

--- Psychic Test/mobile/bin/AndroidManifest.xml.d
+++ Psychic Test/mobile/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Bawb\Documents\GitHub\Psychic Test\mobile\bin\AndroidManifest.xml : \
-C:\Users\Bawb\Documents\GitHub\Psychic Test\mobile\AndroidManifest.xml \
+C:\Users\Bawb\Documents\GitHub\Psychic-Test\Psychic Test\mobile\bin\AndroidManifest.xml : \
+C:\Users\Bawb\Documents\GitHub\Psychic-Test\Psychic Test\mobile\AndroidManifest.xml \
